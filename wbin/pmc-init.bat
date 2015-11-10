053a1b9ea861334646a1dbc95f2e5b36918d1c58
Archana Mandhare <amandhare@pmc.com>
2015-11-10 22:00:48 +0530

    Moved pmc specific scripts to private repo.

--- wbin/pmc-init.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd %~dp0
-call vip-init.bat -domain local.dev
-cd ..
-copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc\hosts.backup
-copy /Y %SystemRoot%\System32\drivers\etc\hosts pmc
-vagrant ssh -c /srv/pmc/setup-sites.sh
-copy pmc\hosts /Y %SystemRoot%\System32\drivers\etc
-popd
--- www/wp-content/mu-plugins/pmc-wwd.php
+++
@@ -1,7 +0,0 @@
-<?php
-define( 'WWD_FAST_JS_HOST','http://uls.vip.local');
-define( 'WWD_PAYWALL_HOST','http://uls.vip.local');
-define( 'WWD_ISSUE_LINK_PREFIX', 'http://dd-wwd.vip.local/issue/local');
-define( 'MOBIFY_BUNDLE_LOCATION', get_template_directory_uri() . '/static/mobify' );
-
-// EOF
